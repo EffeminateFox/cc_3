e451244a3b248e92ecc69b4778449ec422ed0479
EffeminateFox <111376341+EffeminateFox@users.noreply.github.com>
2024-05-19 11:16:29 -0400

    Update cc_3.js
    Organized implementations and added descriptions

--- cc_3.js
+++ cc_3.js
@@ -1,15 +1,25 @@
-// Car.js
 class Car {
+    /**
+     * Creates a new Car instance.
+     * @param {string} make - The brand of the car.
+     * @param {number} speed - The current speed of the car in km/h.
+     */
     constructor(make, speed) {
         this.make = make;
         this.speed = speed;
     }
 
+    /**
+     * Increases the car's speed by 10 km/h and logs the new speed.
+     */
     accelerate() {
         this.speed += 10;
         console.log(`${this.make} is going at ${this.speed} km/h`);
     }
 
+    /**
+     * Decreases the car's speed by 5 km/h and logs the new speed.
+     */
     brake() {
         this.speed -= 5;
         console.log(`${this.make} is going at ${this.speed} km/h`);
@@ -17,3 +27,4 @@ class Car {
 }
 
 module.exports = Car;
+
